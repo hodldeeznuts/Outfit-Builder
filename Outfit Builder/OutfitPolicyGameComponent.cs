6383069fe625b6d8187b45bc922ba2da49531f09
hodlhodl <don@dmsgmail.com>
2019-05-27 22:03:16 -0700

    Init

---
+++ Outfit Builder/OutfitPolicyGameComponent.cs
@@ -0,0 +1,64 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Outfit_Builder
+{
+    public class OutfitPolicyGameComponent : GameComponent
+    {
+        public OutfitPolicyGameComponent(Game game)
+        {
+            List<Pawn> pawnsToRemove = new List<Pawn>();
+
+            foreach (Pawn pawn in storedOutfits.Keys)
+            {
+                if (pawn.DestroyedOrNull())
+                {
+                    pawnsToRemove.Add(pawn);
+                }
+            }
+
+            foreach (Pawn pawn in pawnsToRemove)
+            {
+                storedOutfits.Remove(pawn);
+            }
+        }
+
+        public Outfit OutfitAssignedToPawn(Pawn pawn)
+        {
+            if (storedOutfits == null)
+            {
+                storedOutfits = new Dictionary<Pawn, int>();
+            }
+
+            if (storedOutfits.ContainsKey(pawn))
+            {
+                return Current.Game.outfitDatabase.AllOutfits.Find(s => s.uniqueId == storedOutfits[pawn]);
+            }
+
+            return null;
+        }
+
+        public Outfit CreateNewOutfit(Pawn pawn)
+        {
+            Outfit newOutfit = Current.Game.outfitDatabase.MakeNewOutfit();
+            newOutfit.label = pawn.Name.ToStringShort.CapitalizeFirst();
+
+            storedOutfits.Add(pawn, newOutfit.uniqueId);
+
+            return newOutfit;
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Collections.Look(ref storedOutfits, "storedOutfits", LookMode.Reference, LookMode.Value, ref allPawns, ref outfitIdentifiers);
+        }
+
+        static Dictionary<Pawn, int> storedOutfits = new Dictionary<Pawn, int>();
+        static List<Pawn> allPawns = new List<Pawn>();
+        static List<int> outfitIdentifiers = new List<int>();
+    }
+}
---
+++ Outfit Builder/Outfit_Builder.cs
@@ -0,0 +1,60 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Outfit_Builder
+{
+    public class Outfit_Builder : Mod
+    {
+        public Outfit_Builder(ModContentPack content) : base(content)
+        {
+
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        private static readonly Type patchType = typeof(HarmonyPatches);
+
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("com.github.harmony.rimworld.mod.outfit_builder");
+
+            harmony.Patch(
+                    original: AccessTools.Method(typeof(RimWorld.OutfitForcedHandler), nameof(RimWorld.OutfitForcedHandler.Reset)),
+                    prefix: new HarmonyMethod(type: patchType, name: nameof(OutfitResetPrefix))
+                );
+        }
+
+        static void OutfitResetPrefix(RimWorld.OutfitForcedHandler __instance)
+        {
+            if (__instance.ForcedApparel == null || __instance.ForcedApparel.Count < 1) return;
+
+            OutfitPolicyGameComponent component = Current.Game.GetComponent<OutfitPolicyGameComponent>();
+
+            Pawn pawn = __instance.ForcedApparel[0].Wearer;
+
+            Outfit outfitAssignedToPawn = component.OutfitAssignedToPawn(pawn);
+
+            if (outfitAssignedToPawn == null)
+            {
+                outfitAssignedToPawn = component.CreateNewOutfit(pawn);
+            }
+
+            outfitAssignedToPawn.filter = new ThingFilter();
+
+
+            foreach (Apparel apparel in pawn.apparel.WornApparel)
+            {
+                outfitAssignedToPawn.filter.SetAllow(apparel.def, true);
+            }
+
+            pawn.outfits.CurrentOutfit = outfitAssignedToPawn;
+        }
+    }
+}
